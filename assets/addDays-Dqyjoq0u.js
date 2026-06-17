852bbe92408bb4a0b9126fff11705c818c2080d5
Borja Martin <mituninja@gmail.com>
2026-06-17 20:42:44 +0200

    Deploy to GitHub Pages

---
+++ assets/addDays-Dqyjoq0u.js
@@ -0,0 +1,6 @@
+import{c}from"./index-CO60oDZm.js";import{t as n,h as f}from"./schedule-C27FGU7t.js";/**
+ * @license lucide-react v1.20.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],i=c("chevron-left",s);function N(a,t,r){const e=n(a,r==null?void 0:r.in);return isNaN(t)?f(a,NaN):(t&&e.setDate(e.getDate()+t),e)}export{i as C,N as a};
